c9f8589bd48102a58635652f7850a30f96a41b82
tontonDuPirox <onclebobs@gmail.com>
2013-09-15 13:49:47 +0200

    Actions module
    Not sure if my implementation is good, but it is working

--- actionListFactory.py
+++ actionListFactory.py
@@ -1,15 +1,7 @@
 # -*- coding: utf8 -*-
 
-from actions.about import *
-from actions.four_o_four_checker import *
-from actions.google import *
-from actions.nsfw_gag import *
-from actions.uptime import *
-from actions.ping import *
-from actions.youtube import *
-from actions.quit import *
-from actions.help import *
 from security import *
+import actions
 
 class ActionListFactory(object):
 
@@ -17,36 +9,36 @@ class ActionListFactory(object):
 		actionList = []
 
 		security = Security()		
-		about = About(application, '!about', 'Affiche les crédits', security)
+		about = actions.About(application, '!about', 'Affiche les crédits', security)
 		actionList.append(about)
 
 		security = Security()
-		fourofour = FourOFourChecker(application, 'fourofour', 'Renvoi un message si une url renvoit une erreur 404', security)
+		fourofour = actions.FourOFourChecker(application, 'fourofour', 'Renvoi un message si une url renvoit une erreur 404', security)
 		actionList.append(fourofour)
 
 		security = Security()
-		youtube = Youtube(application, 'youtube', 'Nom / Auteur / Durée de la vidéo', security)
+		youtube = actions.Youtube(application, 'youtube', 'Nom / Auteur / Durée de la vidéo', security)
 		actionList.append(youtube)
 
 		security = Security()
-		nsfw = NSFWGag(application, 'nsfw_gag', 'Signale les posts 9gag NSFW', security)
+		nsfw = actions.NSFWGag(application, 'nsfw_gag', 'Signale les posts 9gag NSFW', security)
 		actionList.append(nsfw)
 		
 		security = Security()
-		uptime = Uptime(application, '!uptime', 'Retourne l\'uptime de ce bot', security)
+		uptime = actions.Uptime(application, '!uptime', 'Retourne l\'uptime de ce bot', security)
 		actionList.append(uptime)
 
 		security = Security()
-		ping = Ping(application, '!ping', 'pong', security)
+		ping = actions.Ping(application, '!ping', 'pong', security)
 		actionList.append(ping)
 
 		security = Security()
-		help = Help(application, '!help', 'Affiche la liste des commandes', security)
+		help = actions.Help(application, '!help', 'Affiche la liste des commandes', security)
 		actionList.append(help)
 
 		security = Security()
 		security.addToWhiteList('tonton')
-		quit = Quit(application, '!quit', 'Quitter', security)
+		quit = actions.Quit(application, '!quit', 'Quitter', security)
 		actionList.append(quit)
 		
 		#
