af827f829e1a3d9580fdd5c728c70e95941164a6
tontonDuPirox <onclebobs@gmail.com>
2013-10-10 23:35:41 +0200

    Adding a setup file
    steel experimental, and not tested yed

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='tonton-bot',
+    version='0.2',
+    description='Small irc bot using Twisted',
+    long_description=readme,
+    author='Tonton duPirox',
+    author_email='onclebobs@gmail.com',
+    url='https://github.com/tontonDuPirox/tonton-bot',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
