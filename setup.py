8f324a5aa405cbe1ac4c8bc0e217dbe0cda240c9
tontonDuPirox <onclebobs@gmail.com>
2013-10-10 23:36:15 +0200

    Beerware license
    This software is released under the beerware licence

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
+with open('LICENSE.txt') as f:
+    license = f.read()
+
 setup(
     name='tonton-bot',
     version='0.2',
@@ -14,6 +17,7 @@ setup(
     author='Tonton duPirox',
     author_email='onclebobs@gmail.com',
     url='https://github.com/tontonDuPirox/tonton-bot',
+    license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
 
