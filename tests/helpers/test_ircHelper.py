6b923ae81fae03ec5a12e0ec07644015c6ed8903
tontonDuPirox <onclebobs@gmail.com>
2013-10-17 18:55:30 +0200

    First unittest
    run py.test to execute

---
+++ tests/helpers/test_ircHelper.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from tontonbot.helpers import irc_helper
+
+
+class TestIrcHelper(TestCase):
+    def test_extract_nickname(self):
+        self.assertEqual(irc_helper.IrcHelper.extract_nickname('tonton!qdmljazo'), 'tonton')
+
+    def test_extract_sender(self):
+        self.assertEqual(irc_helper.IrcHelper.extract_sender('tonton', '#toto', 'tutu'), '#toto')
+        self.assertEqual(irc_helper.IrcHelper.extract_sender('tonton', 'tutu', 'tutu'), 'tonton')
